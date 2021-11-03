a330fd8b192576c75bfba6bf04428e99e4e5b8ad
samir-missaoui <samirmissaoui31@gmail.com>
2021-11-03 21:22:37 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import myWonderfulImage from "./téléchargement.png";
+class App extends Component {
+  constructor() {
+    super();
+    this.FirstName = "Samir";
+    this.LastName = "Missaoui";
+    this.age = 28;
+    this.nation = "Tunisian";
+    this.prof = "engineer and web developer";
+    this.img = myWonderfulImage;
+  }
+  render() {
+    return (
+      <>
+        <button onClick={() => this.setState({ show: !this.state.show })}>
+          TRUE
+        </button>
+        {this.state.show ? (
+          <div style={{ color: "red", fontSize: 30, textAlign: "center" }}>
+            <h1>Welcome in my state checkpoint</h1>
+            <h2>My bio</h2>
+            <p>
+              My name is {this.FirstName} {this.LastName}
+            </p>
+            <p>My age is {this.age}</p>
+            <p>I'm an {this.prof}</p>
+            <p>I'm {this.nation} </p>
+            <img src={this.img} alt="myImage" />
+          </div>
+        ) : null}
+      </>
+    );
+  }
 }
 
 export default App;
